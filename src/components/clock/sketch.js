d0ae19c155f3c1683cdccd795620e0efb904d1f4
Paulette Del Valle <paulette.dvm@gmail.com>
2023-08-23 13:18:49 -0400

    changed circle colors

--- src/components/clock/sketch.js
+++ src/components/clock/sketch.js
@@ -20,10 +20,10 @@ const sketch = (p) => {
     let hr_col = p.color(252, 147, 26);
     let xy_min = draw_circles(hr_total, curr_hr, hr_col, canvasWidth - (canvasWidth - 25), canvasHeight - (canvasHeight - 25));
 
-    let min_col = p.color(24, 157, 194);
+    let min_col = p.color(66, 202, 253);
     let xy_sec = draw_circles(min_total, curr_min, min_col, xy_min[0], xy_min[1]);
 
-    let sec_col = p.color(151, 204, 4);
+    let sec_col = p.color(176, 132, 204);
     draw_circles(sec_total, curr_sec, sec_col, xy_sec[0], xy_sec[1]);
   };
 
