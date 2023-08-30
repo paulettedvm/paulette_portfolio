3b50c17d49cdd49f3a55af26f2a4d070cea6a37b
Paulette Del Valle <paulette.dvm@gmail.com>
2023-08-30 14:45:22 -0400

    removed hover msg bc i didn't like it...

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -7,7 +7,6 @@ const Clock = () => {
     return(
         <div className="clock-wrapper">
             <ReactP5Wrapper sketch={sketch}/>
-            <p className="tooltip">by the way, are you wondering "what the hell are these dots?" e-mail me and i'll tell you! ;)</p>
         </div>
     );
 }
