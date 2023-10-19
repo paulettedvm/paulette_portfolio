b4849ed127fd3ba02a82be740e9b4043b234daac
Paulette Del Valle <paulette.dvm@gmail.com>
2023-10-19 12:54:16 -0400

    added stockpred

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,9 +17,12 @@ const Projects = () => {
             <ProjectCard name="an abstract clock" 
                 description="you were probably wondering 'what's with those circles on the home page?' well, this is it! as part of my spring 2021 data visualization course, i developed an abstract clock using p5.js where i was able to have a hands-on design experience."
                 link="https://github.com/paulettedvm/a0_dataviz"/>
-            <ProjectCard name="mistranslation machine" 
+            <ProjectCard name="mistranslation method (inspired by david cameron's methods in 'flowers of bad')" 
                 description="using gpt2, this python script transforms snippets of text into a fully-formed ai-generated text. which i used as a method for a mis-translation of esmeralda santiago's text 'how to eat a guava'."
                 link=""/>
+            <ProjectCard name="stock price predictor" 
+                description="developed and trained a sequential machine learning model that predicts bitcoin pricing."
+                link="https://github.com/paulettedvm/stock-pred"/>
             <ProjectCard name="this website!" 
                 description="i got to use my react and p5.js skills to bring this idea to life, so i hope you enjoy it!"
                 link=""/>
