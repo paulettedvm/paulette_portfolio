e2db5e92d1ff8c35202e69ec29abeeffdec7ca8a
Paulette Del Valle <paulette.dvm@gmail.com>
2024-05-22 19:27:38 -0400

    MOBILE NAV BAR ALIGNMENT FIXED !!!

--- src/components/home-navbar.js
+++ src/components/home-navbar.js
@@ -1,32 +1,18 @@
 import { Link } from 'react-router-dom';
 
-const linkStyle = {
-  textDecoration: "none", 
-  color: "inherit",
-}
-
-const itemStyle = {
-  margin: '0 2rem 1.5rem 0', /* doing this so the nav-bar elements are left-aligned */
-  padding: 0, 
-}
-
 const NavBar = () => {
-  return(
-    <nav>
-      <ul style={{
-        listStyle: "none", 
-        display: "flex",
-        padding: 0, 
-      }}>
-        <li style={itemStyle}><Link to="/" style={linkStyle}>home</Link></li>
-        <li style={itemStyle}><Link to="/about-me" style={linkStyle}>about me</Link></li>
-        <li style={itemStyle}><Link to="/projects" style={linkStyle}>projects</Link></li>
-        {/* <li style={itemStyle}><a href="https://www.linkedin.com/in/paulettedvm/" style={linkStyle}>linkedin</a></li> */}
-        <li style={itemStyle}><a href="mailto:pd2662@columbia.edu" style={linkStyle}>e-mail me!</a></li>
-        
+  return (
+    <nav className='nav-bar'>
+      <ul className='nav-links'>
+        <li><Link to="/">home</Link></li>
+        <li><Link to="/about-me">about me</Link></li>
+        <li><Link to="/projects">projects</Link></li>
+        {/* <li><a href="https://www.linkedin.com/in/paulettedvm/">linkedin</a></li> */}
+        <li><a href="mailto:pd2662@columbia.edu">e-mail me!</a></li>
+
       </ul>
     </nav>
-  ); 
+  );
 }
 
 export default NavBar; 
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div class="app-container">
       <div class="clock-title">
-        <HomeTitle/>
-        <Clock/>
+        <HomeTitle />
+        <Clock />
       </div>
     </div>
   );
