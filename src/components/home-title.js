0fffecd0e387fe9920ce9d73672ff36687543dde
Paulette Del Valle <paulette.dvm@gmail.com>
2024-05-24 14:45:22 -0400

    minor change in home-title right padding (0px -> 10px)

--- src/components/home-title.js
+++ src/components/home-title.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const HomeTitle = () => {
     return (
-        <div>
+        <div className='home-contents'>
             <h1 style={{ marginBlockStart: 0 }}>hey! i'm paulette.</h1>
             <p> i like putting random, cool things on the internet ;) </p>
             <p><Link to="/projects">what is this?</Link></p>
