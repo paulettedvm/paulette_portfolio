6ee3ec4dc5ec48aa3ad2699ff57afef25191bc20
Paulette Del Valle <paulette.dvm@gmail.com>
2024-05-25 11:27:59 -0400

    updated projects page (added cards, updated projects and descriptions)

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import './project-card.css'; // Adjust the path as necessary
+
 const ProjectCard = (props) => {
-    return(
-        <div>
-            <a href={props.link}>{props.name}</a>
-            <p>{props.description}</p>
+    return (
+        <div className="project-card">
+            <h2 className="project-name">{props.name}</h2>
+            {/* <div className='image-container'>
+                <img
+                    src={props.image}
+                    alt={props.name}
+                    className="project-image"
+                />
+            </div> */}
+            <p className="project-skills">{props.skills}</p>
+            <p className="project-description">{props.description}</p>
         </div>
-    ); 
+    );
 }
 
-export default ProjectCard; 
+export default ProjectCard;
