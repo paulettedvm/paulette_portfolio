6586187b27141e9a8f91c58c611e6657119da303
Paulette Del Valle <paulette.dvm@gmail.com>
2024-06-12 18:04:32 -0400

    again...home page wording

--- src/components/home-title.js
+++ src/components/home-title.js
@@ -4,7 +4,7 @@ const HomeTitle = () => {
     return (
         <div className='home-contents'>
             <h1 style={{ marginBlockStart: 0 }}>hey! i'm paulette.</h1>
-            <p> a writer and a software engineer exploring the intersection of media and technology.</p>
+            <p> a writer and software engineer exploring the intersection of media and technology.</p>
             <p className="learn-more"> learn more <Link to="/about-me"> about me</Link> and <Link to="/projects"> my work </Link></p>
         </div>
     );
